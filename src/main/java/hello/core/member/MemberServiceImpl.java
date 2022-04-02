13144fca400c1ef3dbb271e12be1fb022a104c41
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-04-02 21:27:55 +0900

    patch: MemberServieImpl 주석 추가.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //추상화에도 의존하고 구체화에도 의존해서 문제.
 
     @Override
     public void join(Member member) {
