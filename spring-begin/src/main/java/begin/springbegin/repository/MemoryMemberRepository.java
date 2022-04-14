1f9839ef9648d28969082fcec5152f4bc7d199e0
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-04-14 19:55:50 +0900

    feat: MemoryMemberRepository Test 코드

--- spring-begin/src/main/java/begin/springbegin/repository/MemoryMemberRepository.java
+++ spring-begin/src/main/java/begin/springbegin/repository/MemoryMemberRepository.java
@@ -11,18 +11,22 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Member save(Member member) {
+
         member.setId(++sequence);
         store.put(member.getId(), member);
+
         return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
+
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public Optional<Member> findByName(String name) {
+
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
@@ -30,6 +34,7 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public List<Member> findAll() {
+
         return new ArrayList<>(store.values());
     }
 }
