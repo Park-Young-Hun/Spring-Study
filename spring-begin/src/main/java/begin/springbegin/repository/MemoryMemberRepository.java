f03355c622e3752ce5cb0b2aa45266888ecb28aa
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-05-05 15:06:32 +0900

    feat: 컴포넌트 스캔 예제

--- spring-begin/src/main/java/begin/springbegin/repository/MemoryMemberRepository.java
+++ spring-begin/src/main/java/begin/springbegin/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package begin.springbegin.repository;
 
 import begin.springbegin.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long,Member> store = new HashMap<>();
