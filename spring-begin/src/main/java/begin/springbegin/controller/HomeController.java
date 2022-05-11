5b23bc41149f369d662ed6f8bc2d3ede0af4c252
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-05-11 14:15:01 +0900

    feat: MVC 화면 예제

---
+++ spring-begin/src/main/java/begin/springbegin/controller/HomeController.java
@@ -0,0 +1,13 @@
+package begin.springbegin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
--- spring-begin/src/main/java/begin/springbegin/controller/MemberController.java
+++ spring-begin/src/main/java/begin/springbegin/controller/MemberController.java
@@ -1,8 +1,10 @@
 package begin.springbegin.controller;
 
+import begin.springbegin.domain.Member;
 import begin.springbegin.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +13,19 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
