80a250a016583b7fc8bfc7c78fbe1ba02fd9b701
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-05-14 21:54:29 +0900

    feat: MemberList 예제

--- spring-begin/src/main/java/begin/springbegin/controller/MemberController.java
+++ spring-begin/src/main/java/begin/springbegin/controller/MemberController.java
@@ -3,8 +3,11 @@ package begin.springbegin.controller;
 import begin.springbegin.domain.Member;
 import begin.springbegin.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -27,5 +30,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "member/memberList";
     }
 }
