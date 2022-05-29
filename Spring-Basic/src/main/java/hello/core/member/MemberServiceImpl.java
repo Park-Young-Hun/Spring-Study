ed6beafb968028442a1fe868e5d1727bc5f5e5c2
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-05-29 14:31:33 +0900

    feat: 19강 예제코드

--- Spring-Basic/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Spring-Basic/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    //추상화에도 의존하고 구체화에도 의존해서 문제.
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
